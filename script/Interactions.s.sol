a4f169085718ac2e4c7a42075c988d7cd8b2bce3
theirrationalone <anilkushwaha289@gmail.com>
2023-09-02 18:16:09 +0530

    Interactions integrational Tests 🧪 Performed

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,6 +6,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     uint256 private constant ANVIL_DEFAULT_PRIVATE_KEY =
@@ -34,5 +35,66 @@ contract MintBasicNft is Script {
         address recentBasicNft = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
 
         mintBasicNftUsingConfig(recentBasicNft);
+    }
+}
+
+contract MintMoodNft is Script {
+    uint256 private constant ANVIL_DEFAULT_PRIVATE_KEY =
+        0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+
+    function mintMoodNft(address _recentMoodNft) public {
+        uint256 deployerKey;
+        if (block.chainid == 31337) {
+            deployerKey = ANVIL_DEFAULT_PRIVATE_KEY;
+        } else {
+            deployerKey = vm.envUint("PRIVATE_KEY");
+        }
+
+        vm.startBroadcast(deployerKey);
+        MoodNft(_recentMoodNft).mintNft();
+        vm.stopBroadcast();
+    }
+
+    function mintMoodNftUsingConfig(address _recentMoodNft) internal {
+        mintMoodNft(_recentMoodNft);
+    }
+
+    function run() external {
+        address recentMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+
+        mintMoodNftUsingConfig(recentMoodNft);
+    }
+}
+
+contract FlipMood is Script {
+    uint256 private constant ANVIL_DEFAULT_PRIVATE_KEY =
+        0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+
+    function flipMood(address _recentMoodNft, uint256 _mintedTokenId) public {
+        uint256 deployerKey;
+        if (block.chainid == 31337) {
+            deployerKey = ANVIL_DEFAULT_PRIVATE_KEY;
+        } else {
+            deployerKey = vm.envUint("PRIVATE_KEY");
+        }
+
+        vm.startBroadcast(deployerKey);
+        MoodNft(_recentMoodNft).flipMood(_mintedTokenId);
+        vm.stopBroadcast();
+    }
+
+    function flipMoodUsingConfig(address _recentMoodNft) internal {
+        vm.startBroadcast(ANVIL_DEFAULT_PRIVATE_KEY);
+        MoodNft(_recentMoodNft).mintNft();
+        vm.stopBroadcast();
+
+        uint256 mintedTokenId = (MoodNft(_recentMoodNft).getCurrentTokenId() - 1);
+        flipMood(_recentMoodNft, mintedTokenId);
+    }
+
+    function run() external {
+        address recentMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+
+        flipMoodUsingConfig(recentMoodNft);
     }
 }
--- test/integration/interactionsTest.t.sol
+++ test/integration/interactionsTest.t.sol
@@ -5,19 +5,24 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 
 import {BasicNft} from "../../src/BasicNft.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
 import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
-import {MintBasicNft} from "../../script/Interactions.s.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+import {MintBasicNft, MintMoodNft, FlipMood} from "../../script/Interactions.s.sol";
 
 contract BasicNftTest is Test {
     BasicNft private s_basicNft;
+    MoodNft private s_moodNft;
 
     string private constant PUG_IPFS_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() external {
         DeployBasicNft deployer = new DeployBasicNft();
+        DeployMoodNft mood_deployer = new DeployMoodNft();
 
         s_basicNft = deployer.run();
+        s_moodNft = mood_deployer.run();
     }
 
     function testMintBasicNftRunFunctionWorksCorrectly() public {
@@ -36,5 +41,47 @@ contract BasicNftTest is Test {
 
         assert(s_basicNft.getTokenId() > 0);
         assertEq(keccak256(abi.encodePacked(s_basicNft.tokenURI(0))), keccak256(abi.encodePacked(PUG_IPFS_URI)));
+    }
+
+    function testMintMoodNftRunFunctionWorksCorrectly() public {
+        MintMoodNft mintMoodNft = new MintMoodNft();
+        mintMoodNft.run();
+    }
+
+    function testMintMoodNftWorksCorrectly() public {
+        MintMoodNft mintMoodNft = new MintMoodNft();
+
+        vm.chainId(31337);
+        mintMoodNft.mintMoodNft(address(s_moodNft));
+
+        vm.chainId(11155111);
+        mintMoodNft.mintMoodNft(address(s_moodNft));
+
+        assert(s_moodNft.getCurrentTokenId() > 0);
+    }
+
+    function testFlipMoodWorksCorrectly() public {
+        MintMoodNft mintMoodNft = new MintMoodNft();
+        FlipMood flipMood = new FlipMood();
+
+        vm.chainId(31337);
+        mintMoodNft.mintMoodNft(address(s_moodNft));
+        uint256 mintedTokenId_1 = (s_moodNft.getCurrentTokenId() - 1);
+        flipMood.flipMood(address(s_moodNft), mintedTokenId_1);
+
+        vm.chainId(11155111);
+        mintMoodNft.mintMoodNft(address(s_moodNft));
+        uint256 mintedTokenId_2 = (s_moodNft.getCurrentTokenId() - 1);
+        flipMood.flipMood(address(s_moodNft), mintedTokenId_2);
+    }
+
+    function testMintingAndFlipingWorksInIntegration() public {
+        MintMoodNft mintMoodNft = new MintMoodNft();
+        mintMoodNft.mintMoodNft(address(s_moodNft));
+
+        uint256 mintedTokenId = (s_moodNft.getCurrentTokenId() - 1);
+
+        FlipMood flipMood = new FlipMood();
+        flipMood.flipMood(address(s_moodNft), mintedTokenId);
     }
 }
